8f737d89fa36049d055c1daafe6f84b8aa0fbae1
Samindu <84236005+SxminduCxxrxy@users.noreply.github.com>
2022-08-01 23:16:45 +0530

    Adding Tutorial 4

--- Tutorial-4/Q3.scala
+++ Tutorial-4/Q3.scala
@@ -5,14 +5,29 @@ object tutorial4 {
 
         var names = Array[String]("Benny", "Niroshan", "saman", "Kumara")
 
-        println(formatNames(names(0))(toUpper(_)))
-        println(formatNames(names(1).substring(0,2))(toUpper(_)) + formatNames(names(1).substring(2))(toLower(_)))
-        println(formatNames(names(2))(toLower(_)))
-        println(formatNames(names(3).substring(0,1))(toUpper(_)) + formatNames(names(3).substring(1,5))(toLower(_)) + formatNames(names(3).substring(5))(toUpper(_)))
+        println(formatNames(names(0))()(toUpper(_)))
+        println(formatNames(names(1))(1,2)(toUpper(_)))
+        println(formatNames(names(2))(1)(toLower(_)))
+        println(formatNames(names(3))(1,6)(toUpper(_)))
+
     }
 
-    def formatNames(name:String)(caseFunction:String=>String):String =  {
-        caseFunction(name)
+    def formatNames(name:String)(indexL:Int*)(caseFunction:String=>String):String =  {
+        
+        if (indexL.isEmpty) {
+            return caseFunction(name)
+        } 
+
+        val sArray = name.toCharArray()
+        var temp = ""
+        
+        for (i <- indexL) {
+            temp = caseFunction(sArray(i-1).toString())
+            sArray(i-1) = temp.charAt(0)
+        }
+
+        return sArray.mkString("")
+
     }
 
     def toUpper(string:String):String = string.toUpperCase
